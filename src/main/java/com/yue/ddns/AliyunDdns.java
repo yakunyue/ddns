50be3cbda390f9e1448f38697f41aa0b51756539
yueyakun <yyk_job@163.com>
2021-05-07 14:30:46 +0800

    init

---
+++ src/main/java/com/yue/ddns/AliyunDdns.java
@@ -0,0 +1,167 @@
+package com.yue.ddns;
+
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.alidns.model.v20150109.DescribeSubDomainRecordsRequest;
+import com.aliyuncs.alidns.model.v20150109.DescribeSubDomainRecordsResponse;
+import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordRequest;
+import com.aliyuncs.alidns.model.v20150109.UpdateDomainRecordResponse;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Component
+public class AliyunDdns {
+
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private DefaultProfile profile;
+	private IAcsClient client;
+	private DescribeSubDomainRecordsRequest recordsRequest;
+
+	@Value("${spring.profiles.active}")
+	private String activeProfile;// 环境
+
+	@Value("${aliyun.ddns.regionId}")
+	private String regionId;// 地域ID
+	@Value("${aliyun.ddns.accessKeyId}")
+	private String accessKeyId;// 您的AccessKey ID
+	@Value("${aliyun.ddns.secret}")
+	private String secret;// 您的AccessKey Secret
+
+	@PostConstruct
+	public void init(){
+		//  设置鉴权参数，初始化客户端
+		profile = DefaultProfile.getProfile(regionId, accessKeyId, secret);
+		client = new DefaultAcsClient(profile);
+
+		//指定查询的二级域名
+		recordsRequest = new DescribeSubDomainRecordsRequest();
+		recordsRequest.setSubDomain("cloud.yxb.cool");
+	}
+
+	@Scheduled(initialDelay = 10000, fixedDelay = 180000)//3min
+	public void schedulingMethod() {
+
+		DescribeSubDomainRecordsResponse recordsResponse = this.describeSubDomainRecords(recordsRequest, client);
+		List<DescribeSubDomainRecordsResponse.Record> domainRecords = recordsResponse.getDomainRecords();
+		logger.info("查询解析记录结果，domainRecords:{}", domainRecords);
+		//最新的一条解析记录
+		if (domainRecords.size() != 0) {
+			DescribeSubDomainRecordsResponse.Record record = domainRecords.get(0);
+			//  记录ID
+			String recordId = record.getRecordId();
+			//  记录值
+			String recordsValue = record.getValue();
+			//  当前主机公网IP
+			String currentHostIP = this.getCurrentHostIP();
+			logger.info("当前主机公网IP为：{},当前记录值为：{}", currentHostIP, recordsValue);
+
+			if ("dev".equals(activeProfile)) {
+				logger.info("{}环境不修改",profile);
+				return;
+			}
+
+			if (!currentHostIP.equals(recordsValue)) {
+				//  修改解析记录
+				UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
+				//  主机记录
+				updateDomainRecordRequest.setRR("cloud");
+				//  记录ID
+				updateDomainRecordRequest.setRecordId(recordId);
+				//  将主机记录值改为当前主机IP
+				updateDomainRecordRequest.setValue(currentHostIP);
+				//  解析记录类型
+				updateDomainRecordRequest.setType("A");
+				UpdateDomainRecordResponse updateDomainRecordResponse = this.updateDomainRecord(updateDomainRecordRequest, client);
+				logger.info("updateDomainRecord:{}", updateDomainRecordResponse);
+			}
+		}
+	}
+
+	/**
+	 * 获取主域名的所有解析记录列表
+	 */
+	private DescribeSubDomainRecordsResponse describeSubDomainRecords(DescribeSubDomainRecordsRequest request, IAcsClient client) {
+		try {
+			// 调用SDK发送请求
+			return client.getAcsResponse(request);
+		} catch (ClientException e) {
+			e.printStackTrace();
+			// 发生调用错误，抛出运行时异常
+			throw new RuntimeException();
+		}
+	}
+
+	/**
+	 * 获取当前主机公网IP
+	 */
+	private String getCurrentHostIP() {
+		// 这里使用jsonip.com第三方接口获取本地IP
+		String jsonip = "https://jsonip.com";
+		// 接口返回结果
+		String result = "";
+		BufferedReader in = null;
+		try {
+			// 使用HttpURLConnection网络请求第三方接口
+			URL url = new URL(jsonip);
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			urlConnection.setRequestMethod("GET");
+			urlConnection.connect();
+			in = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result += line;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		// 使用finally块来关闭输入流
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			} catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		//  正则表达式，提取xxx.xxx.xxx.xxx，将IP地址从接口返回结果中提取出来
+		String rexp = "(\\d{1,3}\\.){3}\\d{1,3}";
+		Pattern pat = Pattern.compile(rexp);
+		Matcher mat = pat.matcher(result);
+		String res = "";
+		while (mat.find()) {
+			res = mat.group();
+			break;
+		}
+		return res;
+	}
+
+	/**
+	 * 修改解析记录
+	 */
+	private UpdateDomainRecordResponse updateDomainRecord(UpdateDomainRecordRequest request, IAcsClient client) {
+		try {
+			//  调用SDK发送请求
+			return client.getAcsResponse(request);
+		} catch (ClientException e) {
+			e.printStackTrace();
+			//  发生调用错误，抛出运行时异常
+			throw new RuntimeException();
+		}
+	}
+}
---
+++ src/main/java/com/yue/ddns/DdnsApplication.java
@@ -0,0 +1,38 @@
+package com.yue.ddns;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+public class DdnsApplication {
+
+	/**
+	 * 配置文件路径, 以','分割的字符串. 配置采用覆盖式, 当有多个配置路径, 且包含相同配置属性时, 后者会覆盖前者.
+	 * (windows环境下 /home/...以当前磁盘为根目录)
+	 */
+	public final static String CONFIG_FILES_PATH = "configFilesPath";
+	private static final String WINDOWS = "windows";
+	private static final String LINUX = "linux";
+
+	public static void main(String[] args) {
+		// 默认配置位置
+		String configFilesPath = "classpath:application.yaml";
+		String os = System.getProperty("os.name")
+				.toLowerCase();
+		if (os.contains(WINDOWS)) {
+			// 开发环境配置文件位置，优先级比类路径下的高
+			String userDir = System.getProperty("user.home");//系统用户目录
+			String devPath = userDir + "\\my-localhost-config-center\\ddns\\application.yaml";
+			devPath = devPath.replaceAll("\\\\", "/");
+			configFilesPath = String.join(",", configFilesPath, devPath);
+		} else {
+			// 生产环境配置文件位置，优先级比类路径下的高
+			String prodPath = "file:/mycloud/ddns/application.yaml";
+			configFilesPath = String.join(",", configFilesPath, prodPath);
+		}
+		System.setProperty(CONFIG_FILES_PATH, configFilesPath);
+		org.springframework.boot.SpringApplication.run(DdnsApplication.class, "--spring.config.location=" + configFilesPath);
+
+	}
+}
