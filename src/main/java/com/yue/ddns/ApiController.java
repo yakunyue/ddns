48e13301b8b1324fb90e527365bda1b2b5431db0
yueyakun <yueyakun2017@gmail.com>
2022-01-28 12:17:55 +0800

    改成web服务，增加一个远程唤醒接口

--- src/main/java/com/yue/ddns/ApiController.java
+++ src/main/java/com/yue/ddns/ApiController.java
@@ -1,14 +1,34 @@
 package com.yue.ddns;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
-    @RequestMapping("wol")
-    public String wol(){
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    @Autowired
+    private WakeOnLan wakeOnLan;
+
+    @RequestMapping("/wol")
+    public String wol(String mac, String ip) {
+        if (StringUtils.hasText(mac)) {
+            String[] macArray = mac.split("-");
+            char[] macChars = new char[6];
+            for (int i = 0; i < macArray.length; i++) {
+                int mac_part = Integer.parseInt(macArray[i], 16);
+                macChars[i] = (char) mac_part;
+            }
+            wakeOnLan.send(macChars, ip);
+        } else {
+            wakeOnLan.send(null, ip);
+        }
         return "success";
     }
 }
--- src/main/java/com/yue/ddns/WakeOnLan.java
+++ src/main/java/com/yue/ddns/WakeOnLan.java
@@ -1,24 +1,30 @@
 package com.yue.ddns;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class WakeOnLan {
 
-    private static  char[] b550_MAC = {0x50, 0x2F, 0x9B, 0x28, 0x17, 0x0C};
+    private Logger logger = LoggerFactory.getLogger(this.getClass().getName());
+
+    private static char[] b550_MAC = {0x50, 0x2F, 0x9B, 0x28, 0x17, 0x0C};
 
     /**
-     * 发送UDP广播，实现远程开机，默认目标计算机的MAC地址为：28D2443568A7
+     * 发送UDP广播，实现远程开机，默认取 b550 的MAC地址：50-2F-9B-28-17-0C
      */
-    public void send(char[] mac) {
+    public void send(char[] mac, String ip) {
         if (mac == null || mac.length <= 0) {
             mac = b550_MAC;
         }
-        char[] head = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
+        char[] head = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
         char[] cmd = new char[102];
         // 拼接数据包
@@ -29,9 +35,9 @@ public class WakeOnLan {
         }
         final String cmdStr = new String(cmd);
         try {
-            byte[] data = cmdStr.getBytes("ISO-8859-1");
+            byte[] data = cmdStr.getBytes(StandardCharsets.ISO_8859_1);
             /* 在Java UDP中单播与广播的代码是相同的，要实现具有广播功能的程序只需要使用广播地址即可 */
-            InetAddress inetAddr = InetAddress.getByName("192.168.2.105");
+            InetAddress inetAddr = StringUtils.hasText(ip) ? InetAddress.getByName(ip) : InetAddress.getByName("192.168.2.105");
             int port = 7010;
             // 获取广播socket
             // MulticastSocket client = new MulticastSocket(port);
@@ -40,13 +46,13 @@ public class WakeOnLan {
             DatagramPacket packet = new DatagramPacket(data, data.length, inetAddr, port);
             // 发送魔法包
             for (int i = 0; i < 3; i++) {
-                System.out.println("开始发送魔包");
+                logger.info("开始发送魔包");
                 client.send(packet);
-                System.out.println("发送魔包完成");
+                logger.info("发送魔包完成");
                 Thread.sleep(100);
             }
         } catch (Exception e) {
-            System.out.println(e.getMessage());
+            logger.error("发送魔包失败，errorMessage:{}", e.getMessage());
             e.printStackTrace();
         }
     }
